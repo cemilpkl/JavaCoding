97b62937f549d941e28f6972fae02c1f2e548802
cemilpekel <pekelsdet@gmail.com>
2025-05-21 15:57:40 -0500

    ArrayListDemo

---
+++ src/test/java/ArrayListDemo.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+
+    public static void main(String[] args) {
+        // Create an ArrayList
+        List myList = new ArrayList(); // raw type which is not recommended
+
+        ArrayList myList2 = new ArrayList(); // heterogeneous type which is contains any type of object
+
+        ArrayList<String> list = new ArrayList<>(); //holds only String type objects
+
+
+    }
+}
